076f2df14c6b753dcb15d2f462e2c2b196a468f1
MetallicGoat <the.great.metallic.goat@gmail.com>
2022-03-03 19:51:17 -0500

    Initial Commit

---
+++ src/Components/SocialFollow.js
@@ -0,0 +1,17 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
+
+export default function SocialFollow() {
+    return (
+        <div className="social-container">
+            <a href="https://www.facebook.com/Infinite-Support-4Paws-102356961309518/"
+               className="facebook social">
+                <FontAwesomeIcon icon={faFacebook} size="2x" />
+            </a>
+            <a href=""
+               className="instagram social">
+                <FontAwesomeIcon icon={faInstagram} size="2x" />
+            </a>
+        </div>
+    );
+}
--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,12 @@
+import SocialFollow from "./Components/SocialFollow";
+
 export default function Footer() {
     return (
         <div className="foot">
             <footer>
-                <h2>Footer</h2>
+                <SocialFollow/>
+                <h2>InfiniteSupport4Paws</h2>
+                <p>Copyright <span>©</span> Infinite Support 4Paws</p>
             </footer>
         </div>
     );
